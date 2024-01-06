5a70c218217a78c4b71d00c7dc334a35a7275019
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-01-06 11:58:42 +0000

    chore: `yarn denoify`

--- deno_dist/helper/ssg/index.ts
+++ deno_dist/helper/ssg/index.ts
@@ -1,8 +1,10 @@
 import { Buffer } from "node:buffer";
 import * as path from 'node:path'
+import type { Env, Schema } from '../../index.ts'
+import { replaceUrlParam } from '../../client/utils.ts'
 import { inspectRoutes } from '../../helper/dev/index.ts'
 import type { Hono } from '../../hono.ts'
-import type { Env, Schema } from '../../types.ts'
+import { createMiddleware } from '../factory/index.ts'
 
 interface FileSystemModule {
   writeFile(path: string, data: string | Buffer): Promise<void>
@@ -13,6 +15,21 @@ const generateFilePath = (routePath: string) => {
   const fileName = routePath === '/' ? 'index.html' : routePath + '.html'
   return path.join('./static', fileName)
 }
+
+export type GeneratedStaticPaths = Record<string, string>[]
+export type GenerateStaticPaths = () => GeneratedStaticPaths | Promise<GeneratedStaticPaths>
+
+/**
+ * For define SSG route
+ */
+export const ssgRoute = (generateStaticPaths?: GenerateStaticPaths) => createMiddleware(async (c, next) => {
+  if (!generateStaticPaths) {
+    generateStaticPaths = () => [{}]
+  }
+  const staticPaths = await generateStaticPaths()
+  c.header('x-hono-ssg', JSON.stringify(staticPaths))
+  await next()
+})
 
 export const toSsg = async <
   E extends Env = Env,
@@ -29,15 +46,23 @@ export const toSsg = async <
 
     const url = new URL(route.path, baseURL).toString()
     const response = await app.fetch(new Request(url))
-    const html = await response.text()
+    const forHonoSsgHeaderData = response.headers.get('x-hono-ssg')
+    if (!forHonoSsgHeaderData) {
+      continue
+    }
+    const staticPaths: Record<string, string>[] = JSON.parse(forHonoSsgHeaderData)
+    for (const staticPath of staticPaths) {
+      const replacedUrl = replaceUrlParam(route.path, staticPath)
+      const pageRes = await app.request(new URL(replacedUrl, baseURL).href)
+      const html = await pageRes.text()
 
-    const filePath = generateFilePath(route.path)
-    const dirPath = path.dirname(filePath)
+      const filePath = generateFilePath(replacedUrl)
+      const dirPath = path.dirname(filePath)
+      await fsModule.mkdir(dirPath, { recursive: true })
+      await fsModule.writeFile(filePath, html)
+      console.log(`File written: ${filePath}`)
 
-    await fsModule.mkdir(dirPath, { recursive: true })
-    await fsModule.writeFile(filePath, html)
-    console.log(`File written: ${filePath}`)
+    }
   }
-
   console.log('Static site generation completed.')
 }
